2d5613ad85d301a9c48560333cad08e79a59dd6a
Smart <v8@metahash.org>
2018-06-13 11:24:01 +0000

    2018-06-13 11:24:01 auto commit

---
+++ 0x0019c53904e0795e219d84f0a1b2f41641a46e5da496c13ab6/0x0019c53904e0795e219d84f0a1b2f41641a46e5da496c13ab6.0.js
@@ -0,0 +1,5 @@
+var contExp = new Contract();
+let strDump = '{"items":[{"key":"ammount","type":"number","value":220},{"key":"data","type":"Map","value":[["Jane",100],["Bob",120]]}]}';
+contExp = Object.assign(contExp, makeClass(strDump));
+
+
