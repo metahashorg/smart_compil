bea45a6f793e047f9659c95e6ff8665467f82ffe
Smart <v8@metahash.org>
2018-06-19 09:02:01 +0000

    2018-06-19 09:02:01 auto commit

--- 0x040ed03be4a26fbee44a367ced46e3ae1a169a51aab4129fbf/0x040ed03be4a26fbee44a367ced46e3ae1a169a51aab4129fbf.0.js
+++ 0x040ed03be4a26fbee44a367ced46e3ae1a169a51aab4129fbf/0x040ed03be4a26fbee44a367ced46e3ae1a169a51aab4129fbf.0.js
@@ -1,2 +1,6 @@
 contract.transfer(taxi.sender, 10);
-contract.transfer(user.sender, 20);contractJSON.stringify(contract)JSON.stringify(contract, "", 4)
+contract.transfer(user.sender, 20);contractJSON.stringify(contract)JSON.stringify(contract, "", 4)contract.transfer(taxi.sender, 10);
+JSON.stringify(contract, "", 4)contract.transfer(user.sender, 20);
+JSON.stringify(contract, "", 4)contract.transfer(taxi.sender, 10);
+JSON.stringify(contract, "", 4)contract.transfer(taxi.sender, 10);
+JSON.stringify(contract.balance, "", 4)
